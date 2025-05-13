6317c04fb8625510a931e791c0aa18e70ca7e817
ZoeLocke <oz.locke@gmail.com>
2025-05-13 11:57:04 +0100

    made app responsive, possible fix for keyboard issue

--- start.gd
+++ start.gd
@@ -8,6 +8,8 @@ func _ready():
 	Globals.pronoun_pick = ""
 	$VBoxContainer/VBoxContainer/OptionButton.get_popup().add_theme_font_size_override("font_size", 32)
 
+
+
 func _on_line_edit_text_changed(new_text: String) -> void:
 	if $VBoxContainer/VBoxContainer/OptionButton.selected != -1:
 		$VBoxContainer/Button.disabled = false
@@ -20,3 +22,7 @@ func _on_button_button_up() -> void:
 	Globals.name_pick = $VBoxContainer/VBoxContainer/LineEdit.text
 	Globals.pronoun_pick = $VBoxContainer/VBoxContainer/OptionButton.selected
 	get_tree().change_scene_to_file("res://tester.tscn")
+
+
+func _on_line_edit_focus_entered() -> void:
+	$VBoxContainer/VBoxContainer/LineEdit.editable = true
